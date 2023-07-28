01de708519068aba184341f61c65f0267db79f9e
Truc Nguyen <truc.nguyen@nrel.gov>
2023-07-28 13:39:27 -0600

    revise era5 code with new locations and documentation

--- buildings_bench/data/era5_cds/__init__.py
+++ buildings_bench/data/era5_cds/__init__.py
@@ -1,17 +1,39 @@
-area_coor = {
-    'edinburg' : [55.96, -3.19, 55.95, -3.18],
-    'massachusetts' : [42.41, -71.39, 42.4, -71.38],
+import xarray as xr
+from urllib.request import urlopen
+import cdsapi
+import pandas as pd
+
+# Longitude and latitude limits of some locations used in BuildingsBench
+area_lonlat = {
+    'edinburg': [55.96, -3.19, 55.95, -3.18],
+    'massachusetts': [42.41, -71.39, 42.4, -71.38],
     'portugal': [42, -9.5, 37, -6],
-    'london' : [51.51, -0.12, 51.5,-0.11],
-    'sceaux' : [48.78, 2.29, 48.77, 2.3],
-    'waterloo' : [43.47, -80.52, 43.46, -80.51]
+    'lisbon': [38.74, -9.15, 38.73, -9.14],
+    'london': [51.51, -0.12, 51.5,-0.11],
+    'sceaux': [48.78, 2.29, 48.77, 2.3],
+    'waterloo': [43.47, -80.52, 43.46, -80.51]
 }
 
-def get_hourly_weather_pandas(start_date: str, end_date: str, area: list, variable=['2m_temperature'], download_path=None):
-    import xarray as xr
-    from urllib.request import urlopen
-    import cdsapi
-    import pandas as pd
+def get_hourly_weather_pandas(
+        start_date: str, 
+        end_date: str, 
+        area: list, 
+        variable: list[str] = ['2m_temperature'], 
+        download_path: str = None, 
+        columns_name: list[str] = ['temperature']) -> pd.DataFrame:
+    """Download weather data from ERA5 CDS API given a date range as pandas dataframe
+
+    Args:
+        start_date (str): start date in 'YYYY-MM-DD'
+        end_date (str): end date in 'YYYY-MM-DD'
+        area (list): specify area by longitude and latitude limits [lat_n long_w lat_s long_e]
+        variable (list[str], optional): ERA5 variables to watch. Defaults to ['2m_temperature'].
+        download_path (str, optional): if not None, save raw data to this path. Defaults to None.
+        columns_name (list[str], optional): specify columns' names for the returned pandas dataframe. Defaults to ['temperature'].
+
+    Returns:
+        pandas.DataFrame: Storing weather data indexed by timestamp
+    """    
     
     # start the client
     cds = cdsapi.Client()
@@ -38,7 +60,7 @@ def get_hourly_weather_pandas(start_date: str, end_date: str, area: list, variab
     # retrieves the path to the file
     fl = cds.retrieve(dataset, params)
     
-    if download_path != None:
+    if download_path is not None:
         fn = '_'.join(start_date + end_date + variable) + '_raw.nc'
         fl.download(download_path + '/' + fn)
         print(f"Saved raw file: {download_path + '/' + fn}")
@@ -52,6 +74,6 @@ def get_hourly_weather_pandas(start_date: str, end_date: str, area: list, variab
     
     # to pandas dataframe
     df = mean_ds.to_pandas()
-    df.columns = variable
+    df.columns = columns_name if columns_name is not None else variable
     
     return df
